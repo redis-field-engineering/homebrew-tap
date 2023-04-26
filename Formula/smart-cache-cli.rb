93b2c700b4e680edfeb67a6d3ff84435d497b3f9
slorello89 <steve.lorello@redis.com>
2023-04-26 09:44:54 -0400

    revving smart-cache-cli to v0.0.7

--- Formula/smart-cache-cli.rb
+++ Formula/smart-cache-cli.rb
@@ -1,10 +1,10 @@
 class SmartCacheCli < Formula  
     desc "Redis Smart Cache CLI tool"
     homepage "https://github.com/redis-field-engineering/redis-smart-cache-cli"
-    url "https://github.com/redis-field-engineering/redis-smart-cache-cli/archive/refs/tags/v0.0.6.tar.gz"
-    sha256 "4c4cb803f161abc2ea78c84eb3992501c5c1e0469dcbc0404bc0daf204058e52"
+    url "https://github.com/redis-field-engineering/redis-smart-cache-cli/archive/refs/tags/v0.0.7.tar.gz"
+    sha256 "e125aa0836ce726bbb7ef3249f10b318be3c71602d800e269c7e352d40d3395f"
     license "MIT"
-    version "v0.0.6"
+    version "v0.0.7"
   
     depends_on "go" => :build
   
@@ -14,7 +14,7 @@ class SmartCacheCli < Formula
   
     test do    
         output = shell_output("#{bin}/smart-cache-clie -v")
-        assert_match "Redis Smart Cache CLI Version: v0.0.6", output
+        assert_match "Redis Smart Cache CLI Version: v0.0.7", output
     end
   end
   
